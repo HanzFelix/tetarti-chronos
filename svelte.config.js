27efa398b2501f07dc5e9e7a780f8d9614b9003f
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2025-06-08 09:48:11 +0800

    update3

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,10 @@ import adapter from '@sveltejs/adapter-static';
 export default {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
-		})
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/tetarti-chronos' : ''
+		}
 	}
 };
